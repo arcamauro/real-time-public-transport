8a4edd5f8913ec31d5512405884735ac3f6ad217
Arcangelo Mauro <arcangelo.mauro@proton.me>
2026-09-16 18:25:33 +0200

    Initial commit

---
+++ src/main/java/com/platform/realtimepublictransport/RealTimePublicTransportApplication.java
@@ -0,0 +1,13 @@
+package com.platform.realtimepublictransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimePublicTransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimePublicTransportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platform/realtimepublictransport/RealTimePublicTransportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.realtimepublictransport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimePublicTransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
